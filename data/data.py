a334093ede4fd38501fbc7a7821835c42f143d99
siqihao95 <haosiqica@gmail.com>
2018-06-28 21:34:20 -0400

    Add files via upload

--- data/data.py
+++ data/data.py
@@ -23,25 +23,24 @@ def sample_params(size):
     sampling_freq = np.array([np.random.uniform(5, 10) * 1000 for i in range(size)])
     stretch_factor = np.array([np.random.uniform(1, 10) for _ in range(size)])
     flag = np.array([np.random.uniform(0, 1) for _ in range(size)])
-    #  ipdb.set_trace()
     samples = []
     strings = []
     cqt_specs = []
     for i in range(size):
-        strings.append(karplus_strong.karplus_strong(pitch[i], 2 * sampling_freq[i], stretch_factor[i], 1))
+        strings.append(karplus_strong(pitch[i], 2 * sampling_freq[i], stretch_factor[i], 1))
         samples.append(strings[i].get_samples())
         cqt_spec = cqt_transform.compute_cqt_spec(samples[i]).T
-        cqt_specs.append(pad_zeros(cqt_spec, (cqt_spec.shape[1], cqt_spec.shape[1])))
+        padded_cqt = pad_zeros(cqt_spec, (cqt_spec.shape[1], cqt_spec.shape[1]))
+        cqt_specs.append(padded_cqt)
     cqt_specs = np.array(cqt_specs)
     print(cqt_specs.shape)
     return pitch, sampling_freq, stretch_factor, flag, cqt_specs
-        
+                
 
 def generate_data(file, size):
+    pitch, sampling_freq, stretch_factor, flag, cqt_specs = sample_params(size)
     with open(file, 'wb') as fh:
-        pitch, sampling_freq, stretch_factor, flag, cqt_specs = sample_params(size)
         data_dict = {'parameters' : np.array([pitch, sampling_freq, stretch_factor, flag]).T, 'cqt_spec' : cqt_specs}
-#         fh.write(pkl.dumps(data_dict))
         pkl.dump(data_dict, fh)
     fh.close()
     print(file)
@@ -55,9 +54,9 @@ def read_data(file):
 
 
 def create_datasets():
-    generate_data('eval.pkl', 20)
-    generate_data('test.pkl', 50)
-    generate_data('train.pkl', 50)
+    generate_data('eval.pkl', 100)
+    generate_data('test.pkl', 5000)
+    generate_data('train.pkl', 50000)
 
     
 def read_dataset():
