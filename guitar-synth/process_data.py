fc21e244a6eb7c896a2683f63356dfe5c1f39734
Siqi Hao <skylar.hao@mail.utoronto.ca>
2018-07-07 14:08:56 -0400

    debuging..

--- guitar-synth/process_data.py
+++ guitar-synth/process_data.py
@@ -99,6 +99,7 @@ class Net_pitch_sf(nn.Module):
         self.fc1 = nn.Linear(9 * 108 * 108, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 8)
+        self.s = nn.Sigmoid()
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -107,6 +108,7 @@ class Net_pitch_sf(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+        x = self.s(x)
         return x
     
     
