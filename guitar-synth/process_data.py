073e687416b7be9d60ede3273a4f126627b456da
Siqi Hao <skylar.hao@mail.utoronto.ca>
2018-07-08 00:09:45 -0400

    see performance on training set

--- guitar-synth/process_data.py
+++ guitar-synth/process_data.py
@@ -599,4 +599,4 @@ if __name__ == '__main__':
     #train_data, test_data, val_data, eval_data = load_data_hdf5("pitch_sf_sm")
 
     #train_model(net, train_data, val_data, eval_data, 32, 100, "_pitch_sf_nsp_sg", 5000, 500)
-    test_pitch_sf(net, train_data, 32, "_pitch_sf_nsp_sg_150", 5000ß)
+    test_pitch_sf(net, train_data, 32, "_pitch_sf_nsp_sg_150", 5000)
