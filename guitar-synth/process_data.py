b737375162012c6581b2a672c090719d0cc5ac4d
Siqi Hao <skylar.hao@mail.utoronto.ca>
2018-07-09 17:21:38 -0400

    fixed indentation

--- guitar-synth/process_data.py
+++ guitar-synth/process_data.py
@@ -328,7 +328,7 @@ def evaluate(net, validation_loader, size, factor, fixed=False):
             outputs[:, 5] = outputs[:, 5] * 500 
             labels[:, 5] = labels[:, 5] * 500 
         labels[:, 6] = np.log2(labels[:, 6]) * 100
-	outputs[:, 6] = outputs[:, 6] * 100 
+        outputs[:, 6] = outputs[:, 6] * 100 
         outputs[:, 7] = outputs[:, 7] * factor     
         labels[:, 7] = labels[:, 7] * factor
         loss = criterion(outputs, labels)
@@ -393,7 +393,7 @@ def train_model(net, train_data, val_data, eval_data, batch_size, epochs, suffix
                 outputs[:, 5] = outputs[:, 5] * 500
                 labels[:, 5] = labels[:, 5] * 500 
             labels[:, 6] = np.log2(labels[:, 6]) * 100
-	    outputs[:, 6] = outputs[:, 6] * 100 
+            outputs[:, 6] = outputs[:, 6] * 100 
             outputs[:, 7] = outputs[:, 7] * factor     
             labels[:, 7] = labels[:, 7] * factor
             #print(outputs[:, 0])
