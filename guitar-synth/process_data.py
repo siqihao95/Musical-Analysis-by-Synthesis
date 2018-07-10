7f0d47723b761119137da190e4527053edbd75fe
Siqi Hao <skylar.hao@mail.utoronto.ca>
2018-07-09 21:28:32 -0400

    changed the exponential function

--- guitar-synth/process_data.py
+++ guitar-synth/process_data.py
@@ -328,7 +328,7 @@ def evaluate(net, validation_loader, size, factor, fixed=False):
             outputs[:, 5] = outputs[:, 5] * 500 
             labels[:, 5] = labels[:, 5] * 500 
         #labels[:, 6] = np.log2(labels[:, 6]) * 100
-        outputs[:, 6] = torch.Tensor(np.exp2(outputs[:, 6].detach().cpu().numpy())).to(device)
+        outputs[:, 6] = torch.pow(torch.Tensor([2]), outputs[:, 6])        
         #outputs[:, 6] = outputs[:, 6] * 100 
         outputs[:, 7] = outputs[:, 7] * factor     
         labels[:, 7] = labels[:, 7] * factor
@@ -396,7 +396,7 @@ def train_model(net, train_data, val_data, eval_data, batch_size, epochs, suffix
             #labels[:, 6] = np.log2(labels[:, 6]) * 100
             #outputs[:, 6] = outputs[:, 6] * 100
             
-            outputs[:, 6] = torch.Tensor(np.exp2(outputs[:, 6].detach().cpu().numpy())).to(device)
+            outputs[:, 6] = torch.pow(torch.Tensor([2]), outputs[:, 6])
             print(outputs[:, 6])
             print(labels[:, 6])
             outputs[:, 7] = outputs[:, 7] * factor     
